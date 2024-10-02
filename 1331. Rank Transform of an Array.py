fe648fb6f9d6f89e0a5beff2fdbd125d71ae7500
KT <smartman217@gmail.com>
2024-10-02 11:38:39 +0300

    1331

--- 1331. Rank Transform of an Array.py
+++ 1331. Rank Transform of an Array.py
@@ -2,7 +2,16 @@ from typing import List
 
 
 def arrayRankTransform(arr: List[int]) -> List[int]:
-    pass
+    value_to_rank = {}
+    sorted_unique_numbers = sorted(list(set(arr)))
+    
+    for index in range(len(sorted_unique_numbers)): 
+        value_to_rank[sorted_unique_numbers[index]] = index + 1
+        
+    for index in range(len(arr)): 
+        arr[index] = value_to_rank[arr[index]]
+    
+    return arr
 
 
 print(arrayRankTransform([40,10,20,30])) #[4,1,2,3]
